6d43c7903f6a47440642f496c97f8c11027da090
ULTRAPAIN <1032210157@tcetmumbai.in>
2025-11-09 22:50:10 +0530

    Added Week 1 Folder

---
+++ Week1/Day1/Calculation.java
@@ -0,0 +1,13 @@
+package Week1.Day1;
+
+public class Calculation {
+    public static void main(String args[]){
+        int num1 = 10;
+        int num2 =20;
+        System.out.println("Additon of two number is : " + (num1 + num2));
+        System.out.println("Subtraction of two number is :"+ (num2-num1));
+        System.out.println("Multiplication of two number is :"+ num1*num2);
+        System.out.println("Division of two number is :"+ num2/num1);
+        System.out.println("Modulus of two number is :"+ num2%num1);
+    }
+}
